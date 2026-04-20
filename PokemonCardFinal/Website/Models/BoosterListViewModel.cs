67c0dbf63c0120cd9858f4f9b740a98fc262c794
Hunter Jordan <jordan.hunter0508@gmail.com>
2026-04-20 18:19:17 -0500

    Finished Booster List view

---
+++ PokemonCardFinal/Website/Models/BoosterListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using DataDomain;
+
+namespace Website.Models
+{
+    public class BoosterListViewModel
+    {
+        public List<Booster> Boosters { get; set; }
+        public List<Series> SeriesImagePaths { get; set; }
+    }
+}
